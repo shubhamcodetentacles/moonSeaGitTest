3b9b6bfb9832064de683dd0d0ac9cb74a2e9c44b
Rohan8483 <rohanpawar432@gmail.com>
2022-08-03 13:17:29 +0530

    03-08-2022 moonsea_git merged

--- src/app/components/moonbase/collections/collection/collection.module.ts
+++ src/app/components/moonbase/collections/collection/collection.module.ts
@@ -8,13 +8,10 @@ import { Step2Component } from '../mycollections/step/step3/step2/step2.componen
 import { Step3Component } from '../mycollections/step/step3/step3.component';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
 import { SharedModule } from '../../commanShared/shared/shared.module';
-import {MatTabsModule} from '@angular/material/tabs';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { CollectiondetailsComponent } from '../collectiondetails/collectiondetails.component';
 import { AllcollectionComponent } from '../allcollection/allcollection.component';
-import { MatSelectModule } from '@angular/material/select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { MaterialModule } from 'src/app/material/material.module';
 import { ModalForCreateNftComponent } from '../../create-nft/modal-for-create-nft/modal-for-create-nft.component';
 import { CreateCollectionComponent } from '../../create-nft/create-collection/create-collection.component';
